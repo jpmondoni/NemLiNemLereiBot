588c3e253205d7f469b1ef5d8af65861521a2dfe
CaioWzy <CaioWzy@gmail.com>
2017-09-20 19:53:12 -0300

    :fire: Remove comentário desnecessário

--- Bot.py
+++ Bot.py
@@ -2,13 +2,6 @@
 import click
 from NemLiNemLereiBot import RedditBot
 
-# O cli demora a responder alguns segundos caso eu importe o RedditBot aqui,
-# sei que é uma péssima prática mas não consegui resolver de outra forma e
-# acabei importando antes de instanciar o objeto em cada função, considerando
-# que apenas uma função será executada por processo, logo o RedditBot só será
-# importado uma vez e não deverá haver problemas. Se você conseguir resolver
-# de um jeito DRY, só commitar e pull request! :)
-
 @click.group()
 def cli():
     pass
