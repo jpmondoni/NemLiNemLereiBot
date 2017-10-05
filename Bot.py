41b30a4f00cb55a6ac8274e4985f88925ca91470
CaioWzy <CaioWzy@gmail.com>
2017-10-05 19:16:58 -0300

    :rotating_light: Remove avisos do linter

--- Bot.py
+++ Bot.py
@@ -2,23 +2,29 @@
 import click
 from NemLiNemLereiBot import RedditBot
 
+
 @click.group()
 def cli():
     pass
+
 
 @cli.command(short_help='Watch for new submissions')
 def watch():
     Bot = RedditBot(mode='watch')
     Bot.watch()
+
+
 @cli.command(short_help='Fetch & summarize articles')
 def fetch():
     Bot = RedditBot(mode='fetch')
     Bot.fetch()
 
+
 @cli.command(short_help='Render template & reply')
 def reply():
     Bot = RedditBot(mode='reply')
     Bot.reply()
+
 
 if __name__ == '__main__':
     cli()
